3146634fdff5d83f5ed6d2176b9ec717516b96fb
Nellencr <nellencr@gmail.com>
2023-07-04 10:20:44 +0200

    update solutions

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,5 @@
-export default function getListStudentIds(lists {
-  if (Array.isArray(lists)) {
+export default function getListStudentIds(list) {
+  if (Array.isArray(list)) {
     return list.map((obj) => obj.id);
   }
   return [];
